119aa0ae4da25c6bf000586746472f08b6d58c16
Leonardo Oliveira <poliveira.leonardo@gmail.com>
2026-03-24 17:40:17 -0300

    chore: removes axios and old http clients

--- src/renderer/vite-env.d.ts
+++ src/renderer/vite-env.d.ts
@@ -3,12 +3,6 @@
 import type { AppApi } from '../shared/ipc';
 
 declare global {
-  interface ImportMetaEnv {
-    readonly VITE_PRODUCT_API_URL?: string;
-    readonly VITE_PACKING_API_URL?: string;
-    readonly VITE_RECIPE_API_URL?: string;
-  }
-
   interface ImportMeta {
     readonly env: ImportMetaEnv;
   }

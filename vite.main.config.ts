57031c2ec9f08ab106c8c8211371266390f2850b
Leonardo Oliveira <poliveira.leonardo@gmail.com>
2026-03-24 18:23:33 -0300

    feat: add sqlite3 and sqlite as external dependencies in Vite configuration

--- vite.main.config.ts
+++ vite.main.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   build: {
     rollupOptions: {
+      external: ['sqlite3', 'sqlite'],
       output: {
         entryFileNames: 'main.js',
       },
